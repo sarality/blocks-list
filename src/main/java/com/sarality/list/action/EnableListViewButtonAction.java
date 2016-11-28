65eab290ae8495361cfdc342ef7a8d3180caf4ac
Abhideep Singh <abhi@dothat.in>
2016-11-28 20:58:02 +0530

    Action to enable disable buttons. Might make sense to move this as an argument of selector or something triggered by Selector.

---
+++ src/main/java/com/sarality/list/action/EnableListViewButtonAction.java
@@ -0,0 +1,37 @@
+package com.sarality.list.action;
+
+import android.app.Activity;
+import android.widget.Button;
+
+import com.sarality.action.ActionContext;
+import com.sarality.action.ViewAction;
+
+/**
+ * Toggles the Enable / Disable state of a Button depending of whether an item is selected
+ *
+ * @author abhideep@ (Abhideep Singh)
+ */
+public class EnableListViewButtonAction implements ViewAction {
+
+  private final Button button;
+  private final ListItemSelector selector;
+
+  public EnableListViewButtonAction(Button button, ListItemSelector selector) {
+    this.button = button;
+    this.selector = selector;
+  }
+
+  public EnableListViewButtonAction(Activity activity, int buttonViewId, ListItemSelector selector) {
+    this((Button) activity.findViewById(buttonViewId), selector);
+  }
+
+  @Override
+  public boolean perform(ActionContext actionContext) {
+    if (selector.isSelected()) {
+      button.setEnabled(true);
+    } else {
+      button.setEnabled(false);
+    }
+    return true;
+  }
+}
