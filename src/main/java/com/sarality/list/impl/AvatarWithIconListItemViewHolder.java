74c991332e92c3710649a41022ff844ccdc261b4
Satya <puniani@gmail.com>
2018-04-20 11:09:35 +0530

    avatar with icon view holder separate from avatar viewholder

--- src/main/java/com/sarality/list/impl/AvatarWithIconListItemViewHolder.java
+++ src/main/java/com/sarality/list/impl/AvatarWithIconListItemViewHolder.java
@@ -7,6 +7,6 @@ import android.widget.TextView;
  *
  * @author abhideep@ (Abhideep Singh)
  */
-class AvatarListItemViewHolder extends SimpleListItemViewHolder {
+class AvatarWithIconListItemViewHolder extends IconListItemViewHolder {
   TextView avatarTextView;
 }
