87dc147aa427c9ecaf3bc9c99f4c4a0cbdd1f57d
Abhideep Singh <abhi@dothat.in>
2018-09-20 14:43:39 +0530

    Remove List Item Action

--- src/main/java/com/sarality/list/action/RemoveListItemAction.java
+++ src/main/java/com/sarality/list/action/RemoveListItemAction.java
@@ -22,6 +22,6 @@ public class RemoveListItemAction implements ViewAction {
   @Override
   public boolean perform(ActionContext actionContext) {
     ((ViewGroup) itemView.getParent()).removeView(itemView);
-    return false;
+    return true;
   }
 }
